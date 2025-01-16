bf35a641304651fde9920c702a7a652017af21a9
Fisielu <kubam1996@gmail.com>
2025-01-16 17:23:28 +0100

    Initial commit

---
+++ src/test/java/com/Vistual/ApkaKuba/ApkaKubaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vistual.ApkaKuba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApkaKubaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
